ce28e2290f59ce08a6f3e6bca556f1c5d6de3095
Nihar Sarkar <NIHAR-SARKAR@users.noreply.github.com>
2016-09-08 01:48:32 +0530

    Add files via upload

---
+++ BookList/Form1.Designer.cs
@@ -0,0 +1,332 @@
+﻿namespace BookList
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.materialLabel2 = new MaterialSkin.Controls.MaterialLabel();
+            this.materialSingleLineTextField1 = new MaterialSkin.Controls.MaterialSingleLineTextField();
+            this.materialSingleLineTextField2 = new MaterialSkin.Controls.MaterialSingleLineTextField();
+            this.materialLabel1 = new MaterialSkin.Controls.MaterialLabel();
+            this.materialFlatButton1 = new MaterialSkin.Controls.MaterialFlatButton();
+            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
+            this.materialFlatButton2 = new MaterialSkin.Controls.MaterialFlatButton();
+            this.materialLabel3 = new MaterialSkin.Controls.MaterialLabel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.materialFlatButton3 = new MaterialSkin.Controls.MaterialFlatButton();
+            this.materialLabel4 = new MaterialSkin.Controls.MaterialLabel();
+            this.materialFlatButton4 = new MaterialSkin.Controls.MaterialFlatButton();
+            this.materialLabel5 = new MaterialSkin.Controls.MaterialLabel();
+            this.SuspendLayout();
+            // 
+            // materialLabel2
+            // 
+            this.materialLabel2.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.materialLabel2.AutoSize = true;
+            this.materialLabel2.Depth = 0;
+            this.materialLabel2.Font = new System.Drawing.Font("Roboto", 11F);
+            this.materialLabel2.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(222)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.materialLabel2.Location = new System.Drawing.Point(97, 228);
+            this.materialLabel2.MouseState = MaterialSkin.MouseState.HOVER;
+            this.materialLabel2.Name = "materialLabel2";
+            this.materialLabel2.Size = new System.Drawing.Size(56, 19);
+            this.materialLabel2.TabIndex = 8;
+            this.materialLabel2.Text = "Bought";
+            // 
+            // materialSingleLineTextField1
+            // 
+            this.materialSingleLineTextField1.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.materialSingleLineTextField1.Depth = 0;
+            this.materialSingleLineTextField1.Hint = "";
+            this.materialSingleLineTextField1.Location = new System.Drawing.Point(101, 115);
+            this.materialSingleLineTextField1.MouseState = MaterialSkin.MouseState.HOVER;
+            this.materialSingleLineTextField1.Name = "materialSingleLineTextField1";
+            this.materialSingleLineTextField1.PasswordChar = '\0';
+            this.materialSingleLineTextField1.SelectedText = "";
+            this.materialSingleLineTextField1.SelectionLength = 0;
+            this.materialSingleLineTextField1.SelectionStart = 0;
+            this.materialSingleLineTextField1.Size = new System.Drawing.Size(244, 23);
+            this.materialSingleLineTextField1.TabIndex = 0;
+            this.materialSingleLineTextField1.Text = "Enter a Book Name";
+            this.materialSingleLineTextField1.UseSystemPasswordChar = false;
+            this.materialSingleLineTextField1.Enter += new System.EventHandler(this.materialSingleLineTextField1_Enter);
+            this.materialSingleLineTextField1.Leave += new System.EventHandler(this.materialSingleLineTextField1_Leave);
+            // 
+            // materialSingleLineTextField2
+            // 
+            this.materialSingleLineTextField2.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.materialSingleLineTextField2.Depth = 0;
+            this.materialSingleLineTextField2.Hint = "";
+            this.materialSingleLineTextField2.Location = new System.Drawing.Point(101, 170);
+            this.materialSingleLineTextField2.MouseState = MaterialSkin.MouseState.HOVER;
+            this.materialSingleLineTextField2.Name = "materialSingleLineTextField2";
+            this.materialSingleLineTextField2.PasswordChar = '\0';
+            this.materialSingleLineTextField2.SelectedText = "";
+            this.materialSingleLineTextField2.SelectionLength = 0;
+            this.materialSingleLineTextField2.SelectionStart = 0;
+            this.materialSingleLineTextField2.Size = new System.Drawing.Size(244, 23);
+            this.materialSingleLineTextField2.TabIndex = 1;
+            this.materialSingleLineTextField2.Text = "Enter Author\'s Name";
+            this.materialSingleLineTextField2.UseSystemPasswordChar = false;
+            this.materialSingleLineTextField2.Enter += new System.EventHandler(this.materialSingleLineTextField2_Enter);
+            this.materialSingleLineTextField2.Leave += new System.EventHandler(this.materialSingleLineTextField2_Leave);
+            // 
+            // materialLabel1
+            // 
+            this.materialLabel1.Anchor = System.Windows.Forms.AnchorStyles.Bottom;
+            this.materialLabel1.AutoSize = true;
+            this.materialLabel1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.materialLabel1.Depth = 0;
+            this.materialLabel1.Font = new System.Drawing.Font("Roboto", 11F);
+            this.materialLabel1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(222)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.materialLabel1.Location = new System.Drawing.Point(154, 385);
+            this.materialLabel1.MouseState = MaterialSkin.MouseState.HOVER;
+            this.materialLabel1.Name = "materialLabel1";
+            this.materialLabel1.Size = new System.Drawing.Size(139, 40);
+            this.materialLabel1.TabIndex = 3;
+            this.materialLabel1.Text = "                                \r\n\r\n";
+            // 
+            // materialFlatButton1
+            // 
+            this.materialFlatButton1.Anchor = System.Windows.Forms.AnchorStyles.Bottom;
+            this.materialFlatButton1.AutoSize = true;
+            this.materialFlatButton1.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.materialFlatButton1.Depth = 0;
+            this.materialFlatButton1.Location = new System.Drawing.Point(155, 387);
+            this.materialFlatButton1.Margin = new System.Windows.Forms.Padding(4, 6, 4, 6);
+            this.materialFlatButton1.MouseState = MaterialSkin.MouseState.HOVER;
+            this.materialFlatButton1.Name = "materialFlatButton1";
+            this.materialFlatButton1.Primary = false;
+            this.materialFlatButton1.Size = new System.Drawing.Size(136, 36);
+            this.materialFlatButton1.TabIndex = 2;
+            this.materialFlatButton1.Text = "   Add New Book        ";
+            this.materialFlatButton1.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.materialFlatButton1.UseVisualStyleBackColor = true;
+            this.materialFlatButton1.Click += new System.EventHandler(this.materialFlatButton1_Click);
+            // 
+            // dateTimePicker1
+            // 
+            this.dateTimePicker1.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.dateTimePicker1.CalendarMonthBackground = System.Drawing.SystemColors.ScrollBar;
+            this.dateTimePicker1.Location = new System.Drawing.Point(154, 228);
+            this.dateTimePicker1.Name = "dateTimePicker1";
+            this.dateTimePicker1.Size = new System.Drawing.Size(191, 20);
+            this.dateTimePicker1.TabIndex = 7;
+            // 
+            // materialFlatButton2
+            // 
+            this.materialFlatButton2.Anchor = System.Windows.Forms.AnchorStyles.Bottom;
+            this.materialFlatButton2.AutoSize = true;
+            this.materialFlatButton2.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.materialFlatButton2.Depth = 0;
+            this.materialFlatButton2.Location = new System.Drawing.Point(177, 459);
+            this.materialFlatButton2.Margin = new System.Windows.Forms.Padding(4, 6, 4, 6);
+            this.materialFlatButton2.MouseState = MaterialSkin.MouseState.HOVER;
+            this.materialFlatButton2.Name = "materialFlatButton2";
+            this.materialFlatButton2.Primary = false;
+            this.materialFlatButton2.Size = new System.Drawing.Size(88, 36);
+            this.materialFlatButton2.TabIndex = 9;
+            this.materialFlatButton2.Text = "   My List        ";
+            this.materialFlatButton2.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.materialFlatButton2.UseVisualStyleBackColor = true;
+            this.materialFlatButton2.Click += new System.EventHandler(this.materialFlatButton2_Click);
+            // 
+            // materialLabel3
+            // 
+            this.materialLabel3.Anchor = System.Windows.Forms.AnchorStyles.Bottom;
+            this.materialLabel3.AutoSize = true;
+            this.materialLabel3.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.materialLabel3.Depth = 0;
+            this.materialLabel3.Font = new System.Drawing.Font("Roboto", 11F);
+            this.materialLabel3.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(222)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.materialLabel3.Location = new System.Drawing.Point(176, 457);
+            this.materialLabel3.MouseState = MaterialSkin.MouseState.HOVER;
+            this.materialLabel3.Name = "materialLabel3";
+            this.materialLabel3.Size = new System.Drawing.Size(91, 40);
+            this.materialLabel3.TabIndex = 10;
+            this.materialLabel3.Text = "                    \r\n\r\n";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.Red;
+            this.label1.Location = new System.Drawing.Point(117, 139);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(210, 15);
+            this.label1.TabIndex = 12;
+            this.label1.Text = "*Required Field. Cannot be left blank.";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.Red;
+            this.label2.Location = new System.Drawing.Point(118, 194);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(210, 15);
+            this.label2.TabIndex = 13;
+            this.label2.Text = "*Required Field. Cannot be left blank.";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.Color.LimeGreen;
+            this.label3.Location = new System.Drawing.Point(145, 429);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(158, 15);
+            this.label3.TabIndex = 14;
+            this.label3.Text = "Record Added Successfully!";
+            // 
+            // materialFlatButton3
+            // 
+            this.materialFlatButton3.Anchor = System.Windows.Forms.AnchorStyles.Bottom;
+            this.materialFlatButton3.AutoSize = true;
+            this.materialFlatButton3.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.materialFlatButton3.Depth = 0;
+            this.materialFlatButton3.Location = new System.Drawing.Point(22, 459);
+            this.materialFlatButton3.Margin = new System.Windows.Forms.Padding(4, 6, 4, 6);
+            this.materialFlatButton3.MouseState = MaterialSkin.MouseState.HOVER;
+            this.materialFlatButton3.Name = "materialFlatButton3";
+            this.materialFlatButton3.Primary = false;
+            this.materialFlatButton3.Size = new System.Drawing.Size(140, 36);
+            this.materialFlatButton3.TabIndex = 15;
+            this.materialFlatButton3.Text = "   Add Borrower        ";
+            this.materialFlatButton3.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.materialFlatButton3.UseVisualStyleBackColor = true;
+            this.materialFlatButton3.Click += new System.EventHandler(this.materialFlatButton3_Click);
+            // 
+            // materialLabel4
+            // 
+            this.materialLabel4.Anchor = System.Windows.Forms.AnchorStyles.Bottom;
+            this.materialLabel4.AutoSize = true;
+            this.materialLabel4.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.materialLabel4.Depth = 0;
+            this.materialLabel4.Font = new System.Drawing.Font("Roboto", 11F);
+            this.materialLabel4.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(222)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.materialLabel4.Location = new System.Drawing.Point(21, 457);
+            this.materialLabel4.MouseState = MaterialSkin.MouseState.HOVER;
+            this.materialLabel4.Name = "materialLabel4";
+            this.materialLabel4.Size = new System.Drawing.Size(143, 40);
+            this.materialLabel4.TabIndex = 16;
+            this.materialLabel4.Text = "                                 \r\n\r\n";
+            // 
+            // materialFlatButton4
+            // 
+            this.materialFlatButton4.Anchor = System.Windows.Forms.AnchorStyles.Bottom;
+            this.materialFlatButton4.AutoSize = true;
+            this.materialFlatButton4.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.materialFlatButton4.Depth = 0;
+            this.materialFlatButton4.Location = new System.Drawing.Point(279, 459);
+            this.materialFlatButton4.Margin = new System.Windows.Forms.Padding(4, 6, 4, 6);
+            this.materialFlatButton4.MouseState = MaterialSkin.MouseState.HOVER;
+            this.materialFlatButton4.Name = "materialFlatButton4";
+            this.materialFlatButton4.Primary = false;
+            this.materialFlatButton4.Size = new System.Drawing.Size(151, 36);
+            this.materialFlatButton4.TabIndex = 17;
+            this.materialFlatButton4.Text = "  Borrowed Books    ";
+            this.materialFlatButton4.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.materialFlatButton4.UseVisualStyleBackColor = true;
+            this.materialFlatButton4.Click += new System.EventHandler(this.materialFlatButton4_Click);
+            // 
+            // materialLabel5
+            // 
+            this.materialLabel5.Anchor = System.Windows.Forms.AnchorStyles.Bottom;
+            this.materialLabel5.AutoSize = true;
+            this.materialLabel5.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.materialLabel5.Depth = 0;
+            this.materialLabel5.Font = new System.Drawing.Font("Roboto", 11F);
+            this.materialLabel5.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(222)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.materialLabel5.Location = new System.Drawing.Point(277, 457);
+            this.materialLabel5.MouseState = MaterialSkin.MouseState.HOVER;
+            this.materialLabel5.Name = "materialLabel5";
+            this.materialLabel5.Size = new System.Drawing.Size(155, 40);
+            this.materialLabel5.TabIndex = 18;
+            this.materialLabel5.Text = "                                    \r\n\r\n";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(452, 534);
+            this.Controls.Add(this.materialFlatButton4);
+            this.Controls.Add(this.materialLabel5);
+            this.Controls.Add(this.materialFlatButton3);
+            this.Controls.Add(this.materialLabel4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.materialFlatButton2);
+            this.Controls.Add(this.materialLabel3);
+            this.Controls.Add(this.dateTimePicker1);
+            this.Controls.Add(this.materialFlatButton1);
+            this.Controls.Add(this.materialLabel1);
+            this.Controls.Add(this.materialSingleLineTextField2);
+            this.Controls.Add(this.materialSingleLineTextField1);
+            this.Controls.Add(this.materialLabel2);
+            this.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "My BookList";
+            this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.Form1_FormClosed);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private MaterialSkin.Controls.MaterialLabel materialLabel2;
+        private MaterialSkin.Controls.MaterialSingleLineTextField materialSingleLineTextField1;
+        private MaterialSkin.Controls.MaterialSingleLineTextField materialSingleLineTextField2;
+        private MaterialSkin.Controls.MaterialLabel materialLabel1;
+        private MaterialSkin.Controls.MaterialFlatButton materialFlatButton1;
+        private System.Windows.Forms.DateTimePicker dateTimePicker1;
+        private MaterialSkin.Controls.MaterialFlatButton materialFlatButton2;
+        private MaterialSkin.Controls.MaterialLabel materialLabel3;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private MaterialSkin.Controls.MaterialFlatButton materialFlatButton3;
+        private MaterialSkin.Controls.MaterialLabel materialLabel4;
+        private MaterialSkin.Controls.MaterialFlatButton materialFlatButton4;
+        private MaterialSkin.Controls.MaterialLabel materialLabel5;
+
+
+
+
+
+
+    }
+}
+
---
+++ BookList/Form1.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using MaterialSkin;
+using MaterialSkin.Controls;
+using System.Configuration;
+using System.Data.SqlClient;
+//using raks = System.String;
+
+namespace BookList
+{
+    public partial class Form1 : MaterialForm
+    {        
+        public Form1()
+        {
+            InitializeComponent();
+            label1.Visible = false;
+            label2.Visible = false;
+            label3.Visible = false;
+            var materialSkinManager = MaterialSkinManager.Instance;
+            materialSkinManager.AddFormToManage(this);
+            materialSkinManager.Theme = MaterialSkinManager.Themes.DARK;
+            materialSkinManager.ColorScheme = new ColorScheme(Primary.DeepOrange900, Primary.BlueGrey900, Primary.BlueGrey500, Accent.LightBlue200, TextShade.WHITE);
+        }
+
+        static string a = "Enter a Book Name";
+        static string b = "Enter Author's Name";
+        private void materialSingleLineTextField1_Enter(object sender, EventArgs e)
+        {
+            if (materialSingleLineTextField1.Text == a)
+            {
+                materialSingleLineTextField1.Text = "";
+                label1.Visible = false;
+                label3.Visible = false; 
+            }
+        }
+        
+        private void materialSingleLineTextField2_Enter(object sender, EventArgs e)
+        {
+            if (materialSingleLineTextField2.Text == b)
+            {
+                materialSingleLineTextField2.Text = "";
+                label2.Visible = false;
+                label3.Visible = false;
+            }
+
+        }
+
+        private void materialSingleLineTextField1_Leave(object sender, EventArgs e)
+        {
+            if (materialSingleLineTextField1.Text == "")
+                materialSingleLineTextField1.Text = a;  
+        }
+
+        private void materialSingleLineTextField2_Leave(object sender, EventArgs e)
+        {
+            if (materialSingleLineTextField2.Text == "")
+                materialSingleLineTextField2.Text = b;
+        }
+                
+        protected override void OnPaint(PaintEventArgs e)
+        {
+            base.OnPaint(e);
+            Pen pen = new Pen(Color.DodgerBlue, 4);
+            Rectangle rect = new Rectangle(0, 0, this.Width, this.Height);
+            e.Graphics.DrawRectangle(pen, rect);
+        }
+        
+        private void materialFlatButton2_Click(object sender, EventArgs e)
+        {
+            this.Hide();
+            Form2 f2 = new Form2();
+            f2.ShowDialog();
+            //f2.Dispose();
+        }
+
+        private void Form1_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void materialFlatButton1_Click(object sender, EventArgs e)
+        {
+            if (materialSingleLineTextField1.Text == a && materialSingleLineTextField2.Text == b)
+                {
+                    label1.Visible = true; label2.Visible = true;
+                }
+                else if (materialSingleLineTextField1.Text == a)
+                {
+                    label1.Visible = true; label2.Visible = false;
+                }
+            else if (materialSingleLineTextField2.Text == b)
+                {
+                    label2.Visible = true; label1.Visible = false;
+                }
+                else
+                {
+                    SqlConnection con = new SqlConnection(ConfigurationManager.ConnectionStrings["ConnectionString"].ConnectionString);
+                    SqlCommand cmd = new SqlCommand("INSERT into AddNew(name, author, bought) VALUES (@name, @author, @bought)", con);
+                    cmd.Parameters.Add("@name", SqlDbType.VarChar).Value = materialSingleLineTextField1.Text;
+                    cmd.Parameters.Add("@author", SqlDbType.VarChar).Value = materialSingleLineTextField2.Text;
+                    cmd.Parameters.Add("@bought", SqlDbType.Date).Value = dateTimePicker1.Value.ToShortDateString();
+                    con.Open();
+                    cmd.ExecuteNonQuery();
+                    con.Close();
+                    label3.Visible = true;
+                    materialSingleLineTextField1.Text = a;
+                    materialSingleLineTextField2.Text = b;
+                }
+        }
+
+        private void materialFlatButton3_Click(object sender, EventArgs e)
+        {
+            this.Hide();
+            Form3 f3 = new Form3();
+            f3.ShowDialog();
+            //f3.Dispose();
+        }
+
+        private void materialFlatButton4_Click(object sender, EventArgs e)
+        {
+            this.Hide();
+            Form4 f4 = new Form4();
+            f4.ShowDialog();
+            //f4.Dispose();
+        }
+     }
+}
